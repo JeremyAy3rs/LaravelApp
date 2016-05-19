88a0db0ce1f427bad62c4077d2cb2d9b14683e29
JeremyAy3rs <jrobayers@gmail.com>
2016-05-19 01:53:48 -0400

    Revisions for site migration

--- app/Reservation.php
+++ app/Reservation.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
-    //
+    protected $fillable = ['total_price', 'occupancy','checkin','checkout','name'];
+
+    public function nights()
+    {
+        return $this->hasMany('App\ReservationNight');
+    }
+
+    function Customer(){
+        return $this->belongsTo('App\Customer');
+    }
 }
--- old/functions.js
+++
@@ -1,1 +0,0 @@
-function book(){alert("Thanks for booking your stay with us!");return true;}
